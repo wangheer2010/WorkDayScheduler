d33afa608db5048bc75ad6ee6921e3f88a1d9f4a
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-03-06 21:16:21 -0500

    Changed to a easier function

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,6 @@
 const currentDate = document.getElementById("currentDay");
 // Create a function to get the current date
-var d = new Date();
-
-var month = d.getMonth()+1;
-var day = d.getDate();
-
-var output = d.getFullYear() + '/' +
-    (month<10 ? '0' : '') + month + '/' +
-    (day<10 ? '0' : '') + day;
+var output = moment().format('L'); 
 
 currentDate.innerText = output;
 
